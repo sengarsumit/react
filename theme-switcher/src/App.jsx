e972edca6a70321057301b1ab9ccc57782638828
sumit sengar <87333260+sengarsumit@users.noreply.github.com>
2024-12-13 15:48:16 +0530

    theme (context-api02)

---
+++ theme-switcher/src/App.jsx
@@ -0,0 +1,39 @@
+import Card from "./components/Card"
+import Button from "./components/Button"
+import { useState,useEffect } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+  return (
+    <>
+      <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                       <Button /> 
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                  </div>
+          </div>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
