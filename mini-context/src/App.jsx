c059d61046c98ba58053226a56710528cd568754
sumit sengar <87333260+sengarsumit@users.noreply.github.com>
2024-12-13 13:04:29 +0530

    context api 01

---
+++ mini-context/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+    <UserContextProvider>
+      <h1> react with chai</h1>
+      <Login/>
+      <Profile/>
+      </UserContextProvider>
+    </>
+  )
+}
+
+export default App
